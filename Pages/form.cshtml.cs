e62fd1a28624ac2b777ddf20eb1c49a028ad15cf
John Katumo <jk1015666@wtstudents.wtamu.edu>
2019-03-28 12:49:41 -0500

    INITIAL COMMIT

--- Pages/form.cshtml.cs
+++ Pages/form.cshtml.cs
@@ -5,13 +5,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace CIDM3312_lAB10.Pages
 {
     public class formModel : PageModel
     {
-        private readonly ILogger _log;
-        public formModel(ILogger <formModel> log)
+         private readonly ILogger _log;
+        
+        [BindProperty]
+        [Display(Name="First Name")]
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string FirstName {get;set;}
+         
+        [BindProperty]
+        [Display(Name="Last Name")]
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+
+        public string LastName {get;set;}
+
+        [BindProperty]
+        [Display(Name="Credit Card Number")]
+        [Required]
+        [CreditCard]
+        [StringLength(20)]
+        public string Credit_card_Number{get;set;}
+        
+       public formModel(ILogger <IndexModel> log)
         {
             _log=log;
         }
@@ -21,9 +43,13 @@ namespace CIDM3312_lAB10.Pages
         }
         public void OnPost()
             {
-                   _log.LogInformation("HTTP Post Request");
+                   _log.LogInformation(FirstName);
+                   _log.LogInformation(LastName);
+                   _log.LogInformation(Credit_card_Number);
+
  
             }
+            
 
     }
 }
